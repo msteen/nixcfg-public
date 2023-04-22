6974a136bd9705b3ac03accb58da92afc314f1bb
Matthijs Steen <emailmatthijs@gmail.com>
2023-04-22 22:02:04 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,33 @@
   description = "NixOS configuration for what is shareable publicly";
 
   inputs = {
-    nixcfg.url = "github:msteen/nixcfg.lib";
+    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-stable.follows = "nixos-22_11";
+    nixos-22_11.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+    nixpkgs.follows = "nixos-stable";
+
+    nixcfg = {
+      url = "github:msteen/nixcfg.lib";
+      inputs.nixpkgs.follows = "nixos-unstable";
+    };
+
+    extra-container = {
+      url = "github:erikarvstedt/extra-container";
+      inputs.nixpkgs.follows = "nixos-unstable";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixos-stable";
+      inputs.utils.follows = "extra-container/flake-utils";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixos-unstable";
+      inputs.nixpkgs-stable.follows = "nixos-stable";
+    };
   };
 
   outputs = inputs:
