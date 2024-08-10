d7f37e14b569b290befa52daeeba244f3bbc4334
Matthijs Steen <emailmatthijs@gmail.com>
2024-08-10 14:56:40 +0200

    Some old profiles

---
+++ nixos/profiles/cryptsetup.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+
+{
+  boot.initrd = {
+    availableKernelModules = [
+      "aes_generic"
+      "aes_x86_64"
+      "aes"
+      "af_alg"
+      "algif_skcipher"
+      "blowfish"
+      "cbc"
+      "cryptd"
+      "dm_crypt"
+      "dm_mod"
+      "ecb"
+      "input_leds"
+      "lrw"
+      "serpent"
+      "sha1"
+      "sha256"
+      "sha512"
+      "twofish"
+      "xts"
+    ];
+
+    extraUtilsCommands = ''
+      copy_bin_and_libs ${pkgs.cryptsetup}/bin/cryptsetup
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [ cryptsetup ];
+
+  environment.sudoAliases = [ "cryptsetup" ];
+}
---
+++ nixos/profiles/sedutil.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  boot.kernelParams = [ "libata.allow_tpm=1" ];
+
+  # https://aur.archlinux.org/cgit/aur.git/tree/mkinitcpio.conf.lib?h=sedutil
+  boot.initrd.kernelModules = [
+    "algif_skcipher"
+    "dm-crypt"
+    "loop"
+    "xts"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    mklinuxpba
+    sedutil
+    sedutil-scripts
+  ];
+}
